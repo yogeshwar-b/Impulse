8fc5361f6c992a8ea4822544237de4e5821809a3
Yogeshwar B. <byogeshwarreddy@gmail.com>
2025-07-20 23:27:50 +0530

    Installed and Configured NativeWInd.

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from "expo-router";
-import { Button, View } from "react-native";
+import { Button, Text, View } from "react-native";
+import "../global.css";
 
 export default function Index() {
   return (
@@ -9,6 +10,9 @@ export default function Index() {
         justifyContent: "center",
         alignItems: "center",
       }}>
+      <View className="bg-red-200">
+        <Text className="text-xl font-bold text-blue-500">Impulse!</Text>
+      </View>
       <Link href="/settings" push asChild>
         <Button title="Settings" />
       </Link>
